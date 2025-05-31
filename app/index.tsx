d8ca8d55d13e9d245bd2a3c85471cae2b9fdd8c1
humaidjaffery <humaidjaffery@gmail.com>
2025-05-31 09:36:33 -0700

    reset + tailwind (nativewind) added

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="h-full w-full bg-background flex items-center justify-center gap-5">
+      <Text className="text-5xl text-primary outline outline-5 outline-accent p-4 rounded-md ">Welcome</Text>
+      <Text className="text-3xl text-accent ">Accent Color</Text>
+
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}"
+  ],
+  presets: [
+    require("nativewind/preset")
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#44c3d4",
+        primary: "#ffffff",
+        accent: "#FDE232"
+      }
+    },
+  },
+  plugins: [],
+}
+
