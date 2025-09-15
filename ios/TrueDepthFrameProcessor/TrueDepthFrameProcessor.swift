a5d375271e5443ce881039aa4ffe5594709e5e64
Ayaan Syed <ayaansyed@Ayaans-MacBook-Air.local>
2025-09-14 17:18:19 -0700

    using Xcode for first time, prebuild ios folder, .n and .swfit files added, untested

---
+++ ios/TrueDepthFrameProcessor/TrueDepthFrameProcessor.swift
@@ -0,0 +1,15 @@
+import VisionCamera
+
+@objc(TrueDepthFrameProcessorPlugin)
+public class TrueDepthFrameProcessorPlugin: FrameProcessorPlugin {
+  public override init(proxy: VisionCameraProxyHolder, options: [AnyHashable: Any]! = [:]) {
+    super.init(proxy: proxy, options: options)
+  }
+
+  public override func callback(_ frame: Frame, withArguments arguments: [AnyHashable: Any]?) -> Any? {
+    let buffer = frame.buffer
+    let orientation = frame.orientation
+    // code goes here
+    return "Yo humaid this frame processor is working"
+  }
+}
